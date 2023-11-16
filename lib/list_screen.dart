aecaa89129a3ed98aca6951a2c205c89de3468e1
NITRO\fardin <fardin467@gmail.com>
2023-11-16 20:52:27 +0600

    edit1

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -2,32 +2,74 @@ import 'package:flutter/material.dart';
 
 class ListScreen extends StatelessWidget {
   const ListScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-            ),
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-            ),
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-            ),
-          ]
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
         ),
+        onPressed: () {},
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("asset/images/my_img.png"),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Fardin Ahsan Shafi",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  "fardin@gmail.com",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17.0,
+                  ),
+                ),
+                SizedBox(height: 24),
+                Text(
+                  "My Tasks",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  "3 Tasks",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              //height: 100,
+              color: Colors.white,
+            ),
+          )
+        ],
       ),
     );
   }
